91e3a547f9ae4d87c4366dbed23adc8c198b8e69
Jamaoui Mouad <mouad.jamaoui@gmail.com>
2024-12-07 23:07:43 +0100

    23- ISR with NextJs

--- src/app/product/[productId]/page.js
+++ src/app/product/[productId]/page.js
@@ -1,33 +1,30 @@
 import Product from "@/app/product/[productId]/_components/Product";
+import {notFound} from "next/navigation";
 
+export const revalidate = 10
 export async function generateStaticParams() {
-    const data = await fetch('https://dummyjson.com/products?select=id')
+    const data = await fetch('http://localhost:3000/products')
     const products = await data.json();
 
-    return products['products'].map((product) => ({
+    return products.map((product) => ({
         productId: product.id.toString(),
     }))
 }
 
-export async function generateMetadata({params}) {
-    const {productId} = await params
-    const data = await fetch(`https://dummyjson.com/products/${productId}?select=title`);
+const getProduct = async (productId) => {
+    const data = await fetch(`http://localhost:3000/products?id=${productId}`);
     const product = await data.json();
 
-    return {
-        title: product.title,
-    }
-}
-
-const getProduct = async (productId) => {
-    const data = await fetch(`https://dummyjson.com/products/${productId}?select=id,title,description,category,price,sku,thumbnail`);
-
-    return await data.json();
+    return product[0];
 }
 
 export default async function ProductDetails({params}) {
     const {productId} = await params
-    const product = await getProduct(productId);
+    const product = await getProduct(productId)
+    if(product === undefined) {
+        return notFound()
+    }
+
     return (
         <main className="flex flex-col items-center justify-between">
             <Product product={product}/>
